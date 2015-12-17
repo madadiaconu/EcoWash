3209751b1b7bebc3d3c05d3d6aaf1c700cea1ae5
m.a.diaconu <m.a.diaconu@accenture.com>
2015-12-17 16:50:17 +0200

    Add navigation on CardViews click

--- EcoWash/app/src/main/java/com/myapps/ecowash/ui/MainActivity.java
+++ EcoWash/app/src/main/java/com/myapps/ecowash/ui/MainActivity.java
@@ -1,18 +1,40 @@
 package com.myapps.ecowash.ui;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 import com.myapps.ecowash.R;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Navigate to MyReservations screen
+        findViewById(R.id.myReservations).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToActivity(MyReservationsActivity.class);
+            }
+        });
+
+        //Navigate to MakeReservation screen
+        findViewById(R.id.newReservation).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToActivity(MakeReservationActivity.class);
+            }
+        });
+
+        //Navigate to WashNow screen
+        findViewById(R.id.washNow).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToActivity(WashNowActivity.class);
+            }
+        });
     }
 }
---
+++ EcoWash/app/src/main/java/com/myapps/ecowash/ui/MakeReservationActivity.java
@@ -0,0 +1,14 @@
+package com.myapps.ecowash.ui;
+
+import android.os.Bundle;
+
+import com.myapps.ecowash.R;
+
+public class MakeReservationActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_reservation);
+    }
+}
