379a864bb9c0646bc11dc4d4ca889f47dd152c20
m.a.diaconu <m.a.diaconu@accenture.com>
2015-12-17 15:47:28 +0200

    Add basic UI

---
+++ EcoWash/app/src/main/java/com/myapps/ecowash/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.myapps.ecowash.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.myapps.ecowash.R;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
