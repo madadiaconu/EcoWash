2430fcfa9defee51be7f65cbd62bbfad21e1652e
madadiaconu <maddaaa04@yahoo.com>
2016-01-08 00:33:43 +0200

    Add some design details Apply layout on LoginActivity

--- EcoWash/app/src/main/java/com/myapps/ecowash/ui/LoginActivity.java
+++ EcoWash/app/src/main/java/com/myapps/ecowash/ui/LoginActivity.java
@@ -2,11 +2,13 @@ package com.myapps.ecowash.ui;
 
 import android.os.Bundle;
 
+import com.myapps.ecowash.R;
+
 public class LoginActivity extends BaseActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_login);
     }
 }
