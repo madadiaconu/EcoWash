9d8a57095398679dc98ae6b4f76095fb19322cbf
madadiaconu <maddaaa04@yahoo.com>
2016-01-07 23:50:56 +0200

    Add LoginActivity with basic design

---
+++ EcoWash/app/src/main/java/com/myapps/ecowash/ui/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.myapps.ecowash.ui;
+
+import android.os.Bundle;
+
+public class LoginActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
