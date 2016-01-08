9c6592baa3cc1195b296ff24538ef05202a44f40
madadiaconu <maddaaa04@yahoo.com>
2016-01-08 17:03:47 +0200

    Add getMyReservations method in ParseClient Create ParseSerializer which helps create model objects from ParseObjects

--- EcoWash/app/src/main/java/com/myapps/ecowash/bl/ParseConstants.java
+++ EcoWash/app/src/main/java/com/myapps/ecowash/bl/ParseConstants.java
@@ -6,6 +6,7 @@ public class ParseConstants {
     public static final String WASHING_MACHINE_ID = "washingMachineId";
     public static final String RESERVATION_ID = "reservationId";
 
+    public static final String GET_MY_RESERVATIONS = "getMyReservations";
     public static final String GET_RSERVATIONS = "getReservations";
     public static final String MAKE_RESERVATION = "makeReservation";
     public static final String CANCEL_RESERVATION = "cancelReservation";
---
+++ EcoWash/app/src/main/java/com/myapps/ecowash/util/ParseSerializer.java
@@ -0,0 +1,49 @@
+package com.myapps.ecowash.util;
+
+import com.myapps.ecowash.model.Reservation;
+import com.myapps.ecowash.model.User;
+import com.myapps.ecowash.model.WashingMachine;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParseSerializer {
+
+    public static Reservation buildReservation(ParseObject parseObject){
+        String objectId = parseObject.getObjectId();
+        String date = parseObject.getString("date");
+        int hour = parseObject.getInt("hour");
+        User user = buildUser(parseObject.getParseObject("user"));
+        WashingMachine washingMachine = buildWashingMachine(parseObject.getParseObject("washingMachine"));
+        return new Reservation(objectId,date,hour,user,washingMachine);
+    }
+
+    public static WashingMachine buildWashingMachine(ParseObject parseObject){
+        try {
+            String name = parseObject.fetchIfNeeded().getString("name");
+            int maximumLoad = parseObject.fetchIfNeeded().getInt("maximumLoad");
+            return new WashingMachine(name, maximumLoad);
+        } catch (ParseException ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public static User buildUser(ParseObject parseObject){
+        String objectId = parseObject.getObjectId();
+        String username = parseObject.getString("username");
+        String password = parseObject.getString("password");
+        String email = parseObject.getString("email");
+        return new User(objectId,username,password,email);
+    }
+
+    public static List<Reservation> buildReservationList(List<ParseObject> parseObjectList){
+        List<Reservation> reservationList = new ArrayList<>();
+        for (ParseObject parseObject:parseObjectList){
+            reservationList.add(buildReservation(parseObject));
+        }
+        return reservationList;
+    }
+}
