290f09a6e8037db0e5ac9718b0eaf62e32c0841e
madadiaconu <maddaaa04@yahoo.com>
2016-01-10 19:48:06 +0200

    Add dateToString and getNextDate methods

--- EcoWash/app/src/main/java/com/myapps/ecowash/util/DateHandler.java
+++ EcoWash/app/src/main/java/com/myapps/ecowash/util/DateHandler.java
@@ -2,14 +2,33 @@ package com.myapps.ecowash.util;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateHandler {
 
-    public static String getCurrentDate(){
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private static String format = "dd-MM-yyyy";
+
+    public static Date currentDate(){
+        return new Date();
+    }
+
+    public static String currentDateToString(){
+        DateFormat dateFormat = new SimpleDateFormat(format);
         Date date = new Date();
         return dateFormat.format(date);
+    }
+
+    public static String dateToString(Date date){
+        DateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.format(date);
+    }
+
+    public static Date getNextDate(Date myDate){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(myDate);
+        cal.add(Calendar.DATE, 1); //minus number would decrement the days
+        return cal.getTime();
     }
 
 }
